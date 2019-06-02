78b818d51f694a104497ba356882b027259b80e7
NewGaming <Leoyuntao@gmail.com>
2019-06-02 19:25:15 +0800

    Update resize.js

--- resize.js
+++ resize.js
@@ -10,13 +10,13 @@ else
 }
 
 
-document.addEventListener ("onresize",function() {
-	if (window.innerWidth <= 900 || document.body.clientWidth <= 900)
-	{
-		window.location.href = "https://newgaminggt.github.io/Pizza-Website/index2.html";
-	}
-	else
-	{
-		window.location.href = "https://newgaminggt.github.io/Pizza-Website/index.html";
-	}
-});
+// document.addEventListener ("onresize",function() {
+// 	if (window.innerWidth <= 900 || document.body.clientWidth <= 900)
+// 	{
+// 		window.location.href = "https://newgaminggt.github.io/Pizza-Website/index2.html";
+// 	}
+// 	else
+// 	{
+// 		window.location.href = "https://newgaminggt.github.io/Pizza-Website/index.html";
+// 	}
+// });
